b53bf9cd31323179ae6d62d5410fdec202308c32
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-04-29 13:32:34 +0300

    Migrate to yarn

--- test/EasyLink.test.ts
+++ test/EasyLink.test.ts
@@ -1,4 +1,5 @@
 import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
 import { ethers } from "hardhat";
 import chai from "chai";
 import { PoseidonHasher } from "./poseidonHasher";
