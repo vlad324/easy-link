0c193159c5ff97c759e470d106a55d5500934cf1
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-05-07 22:01:49 +0200

    Add custom fonts

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import Layout from "../components/Layout";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Fonts/>
       <GlobalContextProvider>
         <Layout>
           <Component {...pageProps} />
