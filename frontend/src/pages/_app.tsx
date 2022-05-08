32d45c84c5747298db44fe8bcc77abe147940aed
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-05-08 13:45:40 +0200

    Add app title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { GlobalContextProvider } from "../contexts/GlobalContext";
 import Layout from "../components/Layout";
 import Fonts from "../components/Fonts";
@@ -9,6 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Fonts/>
+      <Head>
+        <title>EasyLink</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+      </Head>
       <GlobalContextProvider>
         <Layout>
           <Component {...pageProps} />
